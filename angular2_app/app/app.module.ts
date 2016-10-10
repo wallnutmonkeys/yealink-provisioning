6096bfbdd79eb53693ae56123cc8185d07d11b7b
Dominic Paquette <dpaquette@ubity.com>
2016-10-09 20:30:47 -0400

    bootstrap t46g-component instead of the quickstart app.component

--- angular2_app/app/app.module.ts
+++ angular2_app/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import { T46gComponent } from './t46g/t46g-component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  declarations: [ T46gComponent ],
+  bootstrap: [ T46gComponent ]
 })
 export class AppModule { }
