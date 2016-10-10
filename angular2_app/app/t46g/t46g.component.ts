720956e9fb711d848b8edd514be57255cd7c652a
Dominic Paquette <dpaquette@ubity.com>
2016-10-10 08:59:33 -0400

    moved implementation to t46g-keys.component

--- angular2_app/app/t46g/t46g.component.ts
+++ angular2_app/app/t46g/t46g.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 't46g-phone',
-    templateUrl : 't46g-component.html',
-    styleUrls: ['t46g-component.css']
+    templateUrl : 't46g.component.html',
+    styleUrls: ['t46g.component.css']
 })
 export class T46gComponent {
-
 }
