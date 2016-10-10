488f981c831e82f7fff76027f5b23934981b2097
Dominic Paquette <dpaquette@ubity.com>
2016-10-10 08:55:30 -0400

    add t46g-directory.component

---
+++ angular2_app/app/t46g/t46g-directory.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 't46g-directory',
+    templateUrl : 't46g-directory.component.html',
+    styleUrls: ['t46g-directory.component.css']
+})
+export class T46gDirectoryComponent {
+
+}
