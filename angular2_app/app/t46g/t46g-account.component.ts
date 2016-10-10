6887fdb79f8c74158dfcb0c80fd1435383c607c8
Dominic Paquette <dpaquette@ubity.com>
2016-10-10 08:54:41 -0400

    add t46g-account.component

---
+++ angular2_app/app/t46g/t46g-account.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 't46g-account',
+    templateUrl : 't46g-account.component.html',
+    styleUrls: ['t46g-account.component.css']
+})
+export class T46gAccountComponent {
+
+    
+}
